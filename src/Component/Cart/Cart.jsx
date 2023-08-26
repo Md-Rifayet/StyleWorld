712d0ae8e8ff30d71509e81e7c8828136104449d
Rifayet khan Prince <rifayetkhanprince30@gmail.com>
2023-08-26 23:06:56 +0600

    first commit

---
+++ src/Component/Cart/Cart.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+
+const Cart = ({cart, handelRemoveCart}) => {
+
+  let message;
+
+  if(cart.length === 0){
+    message = <div><p>Please add Some Product</p> 
+    <h5>Bye more than 500USD, get special discount</h5>
+    </div>
+    }
+    else{
+      message = <h5>Bye more than 500USD, get special discount</h5>;
+    }
+
+  return (
+    <div>
+        <h2>Order Summary:{cart.length}</h2>
+        {message}
+        {
+          cart.map(tshirt => <p key={tshirt._id}>
+            {tshirt.name}
+            <button onClick={() => handelRemoveCart(tshirt._id)}>X</button>
+          </p> )
+        }
+    </div>
+  )
+}
+
+export default Cart
---
+++ src/Component/Home/Home.jsx
@@ -0,0 +1,50 @@
+import React, { useState } from 'react'
+import { useLoaderData } from 'react-router-dom'
+import TShirtData from '../TShirtData/TShirtData';
+import Cart from '../Cart/Cart';
+import toast from 'react-hot-toast';
+import './Home.css'
+ 
+const Home = () => {
+  const tshirts = useLoaderData();
+
+  const [cart, setCart] = useState([])
+
+
+  const handelAddToCart = tshirt =>{ 
+
+    const exist = cart.find(ts => ts._id === tshirt._id);
+      if(exist){
+          toast("You have already a Tshirt")
+      }
+      else{
+        const newCart = [...cart, tshirt];
+        setCart(newCart)
+      }
+  }
+
+const handelRemoveCart = (id) =>{
+   const remaining =cart.filter(ts => ts._id !== id);
+   setCart(remaining)
+}
+
+  return (
+    <div className='home-container'>
+    
+    <div className='tshirts-container'>
+    {
+      tshirts.map(tshirt => <TShirtData
+       key={tshirt._id}
+       tshirt={tshirt}
+       handelAddToCart={handelAddToCart}
+       ></TShirtData>)
+    }
+    </div>
+    <div className="cart-container">
+      <Cart handelRemoveCart={handelRemoveCart} cart={cart} key={cart._id}></Cart>
+    </div>
+    </div>
+  )
+}
+
+export default Home
